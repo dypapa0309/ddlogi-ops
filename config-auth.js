c6c61a7b3a00e98750a0848a013fd984198d5f28
이상빈 <sangbinsmacbook@isangbins-MacBook-Air.local>
2026-02-17 07:37:53 +0900

    dd

---
+++ config-auth.js
@@ -0,0 +1,4 @@
+window.DDLOGI_CONFIG = {
+  supabaseUrl: "https://hfkxuzxrajprxpegvurs.supabase.co",
+  supabaseKey: "여기에 sb_publishable_9SU-I2_iTE5qTvVT0c-lEw_Hkc-5POqeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhma3h1enhyYWpwcnhwZWd2dXJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExMzI2MzgsImV4cCI6MjA4NjcwODYzOH0._WejRCLVaSmfLdmqXeyb7S27uyYayrd9MaySj6bRV84 anon(public) 키(eyJ...로 시작) 넣기"
+};
