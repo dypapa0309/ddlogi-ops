5b95cbb232567c2783f2bd26f36f017c22c48dd8
이상빈 <sangbinsmacbook@isangbins-MacBook-Air.local>
2026-02-17 07:17:35 +0900

    dd

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 // /config.js
-// ✅ 배포/로컬 공통 설정
-// ⚠️ supabaseUrl, supabaseKey는 너가 쓰는 값으로 넣어줘
-
 window.DDLOGI_CONFIG = {
   supabaseUrl: "https://hfkxuzxrajprxpegvurs.supabase.co",
   supabaseKey: "sb_publishable_9SU-I2_iTE5qTvVT0c-lEw_Hkc-5POqeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhma3h1enhyYWpwcnhwZWd2dXJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExMzI2MzgsImV4cCI6MjA4NjcwODYzOH0._WejRCLVaSmfLdmqXeyb7S27uyYayrd9MaySj6bRV84",
